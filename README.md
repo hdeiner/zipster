94361b8abd04059b056b52a10e45468d64acdf24
howarddeiner <howard.deiner@deinersoft.com>
2019-10-25 15:59:25 -0400

    Initial commit.

---
+++ README.md
@@ -0,0 +1,24 @@
+How to create and test a modern REST API in a server farm environment.
+
+Let's say we want to design a REST API that can take a zipcode and a radius and return all the zipcodes within a certain number of miles to the given cente.
+
+Architecturally, we will use the following framworks and tools:
+
+- Java (for our code)
+- Spark (for running a small REST server)
+- MySQL (for persistence of a zipcode database)
+- FlyWay (to version control the database)
+- Wiremock (for designing and validating the API's contract)
+- Cucumber for Java (to run our integration tests in)
+    - locally in Docker containers (WireMock, Spark, MySQL)
+    - in cloud (using Terraform)
+- Vault will also be a vital component of this project.  We will use it for holding together environments, the endpoints in those environments,and the secrets needed for those environments.
+
+
+TO DO:
+- WireMock stimuli and responses
+- Gherkin for holding the tests
+- enough glue code to run Cucumber
+- the Java code itself for Spark 
+
+I still have an issue with the choice of MySQL, as it stores it's data in a Docker Volume.  That means that I can't store the database and the data as an easy to use image.  I am forced to build it from it's "source" every time I want to use it.
---
+++ step_1_create_zipster-mysql_container.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+
+figlet -w 160 -f standard "Create zipster-mysql Container"
+
+figlet -w 160 -f small "Create MySQL Data Container"
+#docker rmi -f howarddeiner/zipster-mysql-data
+#docker build src/iac/docker-mysql-data/ -t howarddeiner/zipster-mysql-data
+
+figlet -w 160 -f small "Bring Up MySQL Continer"
+docker-compose -f docker-compose-mysql.yml up -d
+
+figlet -w 160 -f small "Wait for MySQL to Start"
+while true ; do
+  result=$(docker logs mysql 2>&1 | grep -c "Version: '5.7.28'  socket: '/var/run/mysqld/mysqld.sock'  port: 3306  MySQL Community Server (GPL)")
+  if [ $result != 0 ] ; then
+    echo "MySQL has started"
+    break
+  fi
+  sleep 5
+done
+
+figlet -w 160 -f small "Ready MySQL for FlyWay"
+echo "CREATE USER 'FLYWAY' IDENTIFIED BY 'FLWAY';" | mysql -h 127.0.0.1 -P 3306 -u root --password=password  zipster > /dev/null
+
+figlet -w 160 -f small "Create Our Database"
+./flyway-4.2.0/flyway info
+./flyway-4.2.0/flyway migrate
+./flyway-4.2.0/flyway info
+
+#figlet -w 160 -f standard "Create zipster-mysql Image"
+
+#figlet -w 160 -f small "Commit and Push the MySQL Data Container to Store the Image"
+#docker stop mysql-data
+#docker rmi -f howarddeiner/zipster-mysql-data
+#docker commit mysql-data howarddeiner/zipster-mysql-data
+#docker login
+#docker push howarddeiner/zipster-mysql-data
+#
+#figlet -w 160 -f small "Commit and Push the MySQL Container to Store the Image"
+#docker stop mysql
+#docker rmi -f howarddeiner/zipster-mysql
+#docker commit mysql howarddeiner/zipster-mysql
+#docker login
+#docker push howarddeiner/zipster-mysql
+#
+#figlet -w 160 -f small "Tear Down MySQL Container"
+#docker-compose -f docker-compose-mysql.yml down
+#
+#sudo rm -rf ./mysql-data
